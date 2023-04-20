9daef7ab561a4aaf2c5f7a37651a97ec53da56cf
Alexandr Deina <depecheneid@gmail.com>
2023-04-20 18:06:07 +0300

    gh pages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,12 @@ function Footer() {
       <div className="footer-copyright">
         <div className="container">
           © {new Date().getFullYear()} Copyright Text
-          <a className="grey-text text-lighten-4 right" href="#!">
+          <a
+            className="grey-text text-lighten-4 right"
+            href="https://github.com/Alex-Deina/React-Shop"
+            target="_blank"
+            rel="noreferrer"
+          >
             Repo
           </a>
         </div>
